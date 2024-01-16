7c9028f9db7b64c98fbf0a078fd4176b65a8b7dc
sunkangchao <sunkangchao@codemao.cn>
2024-01-16 11:45:37 +0800

    [algorithm] feat: encode an n-ary tree to a binary tree.

--- src/study/base/ParentTreeNode.java
+++ src/study/base/ParentTreeNode.java
@@ -10,6 +10,9 @@ public class ParentTreeNode {
     public int val;
     public ParentTreeNode left;
     public ParentTreeNode right;
+    /**
+     * 相比二叉树，多一个指向父节点的指针
+     */
     public ParentTreeNode parent;
 
     public ParentTreeNode(int val) {
