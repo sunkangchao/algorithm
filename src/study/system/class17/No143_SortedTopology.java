63a81a553b4f8325096a723fbfef18c56cc84133
sunkangchao <928893296@qq.com>
2024-09-22 00:16:02 +0800

    [algorithm] feat: 泛型反射获取类型 2024.9.22

--- src/study/system/class17/No143_SortedTopology.java
+++ src/study/system/class17/No143_SortedTopology.java
@@ -3,6 +3,9 @@ package study.system.class17;
 import study.system.baseclass.graph.Graph;
 import study.system.baseclass.graph.Node;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.*;
 
 /**
@@ -39,6 +42,39 @@ public class No143_SortedTopology {
             }
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        No143_SortedTopology obj = new No143_SortedTopology();
+        obj.transformAttrListToMap(list);
+
+    }
+
+    public Map<String, String> transformAttrListToMap(List<?> orderAttributes) {
+        Map<String, String> attMap = new HashMap<>(16);
+
+        Method curMethod;
+        try {
+            // 必须要指定具体参数类型是哪些，否则就会查找无参方法
+            curMethod = No143_SortedTopology.class.getMethod("transformAttrListToMap", List.class);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+        Type[] genericParameterTypes = curMethod.getGenericParameterTypes();
+        if (genericParameterTypes[0] instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) genericParameterTypes[0];
+            Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
+            String typeName = actualTypeArguments[0].getTypeName();
+            System.out.println();
+
+        }
+        return attMap;
+
+        // 总结
+        // 通过发射获取的泛型的具体类型，只能是在编译阶段已经明文书写的类型，如果写的是?，返回的便是?；如果写的是T，返回的便是T。
+        // 为什么 ？ 其实你想想也知道，它这些信息都是基于Class的，与当前对象无关，而当前类的类型参数实例化是处于类实例化阶段的，那么它Class能获取到的就只能说明文写在类或方法等签名上的明文信息。
     }
 
 }
