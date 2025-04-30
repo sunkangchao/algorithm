e9071453616530fa9f152980c7b4735ead7e00a2
sunkangchao <sunkangchao@codemao.cn>
2025-04-30 20:08:09 +0800

    No44_8_MyAtoi。

---
+++ src/main/java/leetcode/top100/No44_8_MyAtoi.java
@@ -0,0 +1,74 @@
+package leetcode.top100;
+
+/**
+ * No44_8_MyAtoi
+ * <p>
+ *     8. 字符串转换整数 (atoi)
+ * </p>
+ *
+ * @author sunkangchao
+ * @version 1.0
+ * @since <pre>04月 30, 2025</pre>
+ */
+public class No44_8_MyAtoi {
+
+
+    public int myAtoi(String s) {
+
+        if (s.isEmpty()) {
+            return 0;
+        }
+
+        char[] chars = s.toCharArray();
+        int i = 0;
+        long ans = 0;
+        boolean flag = true;
+        // 1. 去除前导空格
+        while (i < chars.length && chars[i] == ' ') {
+            i++;
+        }
+
+        if (i >= chars.length) {
+            return 0;
+        }
+
+        // 2. 判断正负
+        if (chars[i] == '-' || chars[i] == '+') {
+            flag = chars[i] == '+';
+            i++;
+        }
+
+        // 3. 去除前导0
+        while (i < chars.length && Character.isDigit(chars[i])) {
+            ans = ans * 10 + (chars[i] - '0');
+            i++;
+
+            long temp = ans;
+            if (!flag) {
+                // 如何取反
+                temp = -ans;
+                if (temp < Integer.MIN_VALUE) {
+                    return Integer.MIN_VALUE;
+                }
+            } else {
+                if (temp > Integer.MAX_VALUE) {
+                    return Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        // 4. 读取到下一个非数字字符或字符串结尾
+        return flag ? (int) ans : (int) -ans;
+
+        // 5. 默认值返回0，即
+
+    }
+
+    public static void main(String[] args) {
+        No44_8_MyAtoi obj = new No44_8_MyAtoi();
+        int ans = obj.myAtoi("+1");
+        System.out.println(ans);
+
+    }
+
+}
